b85eb5206f02f662165bd625b8f1cebc39540f50
Christopher Koning <chris@autocloud.dev>
2020-10-23 11:50:36 -0700

    feat: add module

---
+++ main.tf
@@ -0,0 +1,92 @@
+locals {
+  ###
+  # AutoCloud AWS Account IDs
+  # 
+  # AWS Accounts where AutoCloud tooling runs, and where cross-account access will originate from
+  autocloud_accounts = [
+    "204762158545",
+    "824781544411"
+  ]
+
+  ###
+  # External ID
+  # 
+  # Define the external ID that AutoCloud will use to access your AWS account
+  external_id = "autocloud-link-${var.autocloud_organization_id}"
+
+  ###
+  # IAM Policies
+  # 
+  # Select the IAM policies this cross-account role will have
+  iam_policies = concat(
+    # Include AWS predefined ReadOnly policy?
+
+    var.include_read_only == true ? [] : [],
+
+    # Include AWS predefined SecurityAudit policy?
+    var.include_security_audit == true ? [] : [],
+
+    # Add additional user-specified IAM policy ARNs to attach to this role
+    var.additional_policy_arns
+  )
+}
+
+###
+# IAM Role Trust Policy
+# 
+# Allow AutoCloud accounts to assume this IAM role with the correct external ID
+data "aws_iam_policy_document" "autocloud_access_role_assume_policy" {
+  count = var.enabled == true ? 1 : 0
+
+  statement {
+    effect = "Allow"
+
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = local.autocloud_accounts
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "sts:ExternalId"
+
+      values = [
+        local.external_id
+      ]
+    }
+  }
+}
+
+###
+# IAM Role
+# 
+# IAM role granting AutoCloud cross-account access to your AWS account with the permissions
+# granted above
+resource "aws_iam_role" "autocloud_access_role" {
+  count = var.enabled == true ? 1 : 0
+
+  name = var.iam_role_name
+  path = var.path
+
+  description = var.description
+
+  assume_role_policy    = data.aws_iam_policy_document.autocloud_access_role_assume_policy[0].json
+  force_detach_policies = var.force_detach_policies
+  max_session_duration  = var.max_session_duration
+  permissions_boundary  = var.permissions_boundary
+
+  tags = var.tags
+}
+
+###
+# IAM Policy Attachments
+# 
+# Attach all given the policies with the IAM role
+resource "aws_iam_role_policy_attachment" "autocloud_access_role_policy_attachments" {
+  for_each = var.enabled == true ? local.iam_policies : []
+
+  role       = data.aws_iam_policy_document.autocloud_access_role_assume_policy[0].json
+  policy_arn = each.value
+}
---
+++ variables.tf
@@ -0,0 +1,70 @@
+variable "autocloud_organization_id" {
+  type        = "string"
+  description = "AutoCloud organization ID"
+}
+
+variable "include_read_only" {
+  type        = bool
+  default     = true
+  description = "Whether or not to add the AWS predefined ReadOnly policy to the IAM role"
+}
+
+variable "include_security_audit" {
+  type        = bool
+  default     = true
+  description = "Whether or not to add the AWS predefined SecurityAudit policy to the IAM role"
+}
+
+variable "additional_policy_arns" {
+  type        = list(string)
+  default     = []
+  description = "ARNs for any additional IAM policies to attach to the role"
+}
+
+variable "enabled" {
+  type        = bool
+  default     = true
+  description = "Whether or not to create the IAM role"
+}
+
+variable "iam_role_name" {
+  type        = string
+  default     = "AutoCloudReadOnly"
+  description = "Name of the IAM role"
+}
+
+variable "path" {
+  type        = string
+  default     = "/"
+  description = "Path of the IAM role"
+}
+
+variable "description" {
+  type        = string
+  default     = "AutoCloud cross-account access role"
+  description = "Description for the IAM role"
+}
+
+variable "force_detach_policies" {
+  type        = bool
+  default     = false
+  description = "Specifies to force detaching any policies the role has before destroying it"
+}
+
+variable "max_session_duration" {
+  type        = number
+  default     = 3600
+  description = "The maximum session duration (in seconds) that you want to set for the role"
+}
+
+variable "permissions_boundary" {
+  type        = string
+  default     = null
+  description = "The ARN of the policy that is used to set the permissions boundary for the role"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Key-value map of tags for the IAM role"
+}
